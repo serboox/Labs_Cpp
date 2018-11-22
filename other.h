a25db06cd47085beefe1b88a7e1aa7dc1c000117
Sergey Bukharkin <buharkin@selectel.ru>
2018-11-22 21:42:27 +0300

    Middle commit

--- other.h
+++ other.h
@@ -1,8 +1,21 @@
 
 //Прототипы используемых в данном задании функций:
 
-void Sort(char* pcFirst, int nNumber, int size,
-	 void (*Swap)(void*, void*), int (*Compare)(void*, void*) );
-void SwapInt(void* p1, void* p2);
-int CmpInt(void* p1, void* p2);
+void Sort(char *pcFirst, int nNumber, int size,
+          void (*Swap)(void *, void *), int (*Compare)(void *, void *));
+
+void SwapInt(void *p1, void *p2);
+
+int CmpInt(void *p1, void *p2);
+
+struct BOOK {
+    std::string authorFirstName; // 24
+    std::string authorLastName; // 24
+
+    std::string bookTitle; // 24
+    short int bookYear; // 2
+    /* 2 padding byte */
+    float bookPrice; // 4
+    std::string bookCategory; // 24
+};
 
