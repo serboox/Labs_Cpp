5e418da5929aff4be3a9aac261d2a92c8925ec5c
Sergey Bukharkin <serboox@gmail.com>
2018-12-18 03:03:15 +0300

    Finished two assignments

--- other.cpp
+++ other.cpp
@@ -1,22 +1,16 @@
 
-#include <cstdio>
-#include <string>
-#include <vector>
-
 #include "book.h"
 #include "other.h"
 
-
 void run()
 {
-	std::vector<BOOK> bookVector;
-
+	Card card;
 	while (true) {
 		std::string command;
 		std::printf("$ ");
 
 		std::getline(std::cin, command);
-		std::printf(u8"---%s----%d--\n", command.c_str(), command.length());
+		std::printf("---%s----%d--\n", command.c_str(), command.length());
 
 		if (!command.compare(COMMAND_HELP)) {
 			std::printf(
@@ -25,6 +19,7 @@ void run()
 				"%s  Удалить существующую(ие). \n"
 				"%s  Записать текущее содержимое картотеки в файл. \n"
 				"%s  Считать из файла содержимое в картотеку. \n"
+				"%s  Отобразить список комманд. \n"
 				"%s  Выйти. \n",
 				COMMAND_PRINT,
 				COMMAND_SET,
@@ -36,19 +31,23 @@ void run()
 			);
 		}
 		else if (!command.compare(COMMAND_PRINT)) {
-			std::printf(" Распечатать содержимое картотеки. \n");
+			std::printf("Cодержимое картотеки: \n\n");
+			card.print();
+			std::printf("\n");
 		}
 		else if (!command.compare(COMMAND_SET)) {
-			std::printf(" Ввести новую книгу \n");
+			std::printf("Ввести новую книгу \n\n");
+			card.set();
+			std::printf("\n");
 		}
 		else if (!command.compare(COMMAND_DELETE)) {
-			std::printf(" Удалить существующую(ие). \n");
+			std::printf("Удалить существующую(ие). \n");
 		}
 		else if (!command.compare(COMMAND_WRITE)) {
-			std::printf(" Записать текущее содержимое картотеки в файл. \n");
+			std::printf("Записать текущее содержимое картотеки в файл. \n");
 		}
 		else if (!command.compare(COMMAND_READ)) {
-			std::printf(" Считать из файла содержимое в картотеку. \n");
+			std::printf("Считать из файла содержимое в картотеку. \n");
 		}
 		else if (!command.compare(COMMAND_EXIT)) {
 			break;
@@ -66,4 +65,89 @@ void run()
 			std::cin.ignore();
 		}
 	}
+}
+
+void Card::set()
+{
+	BOOK newBook;
+	newBook.fillFromStdIn();
+	this->bookVector.push_back(newBook);
+}
+
+void Card::print()
+{
+	size_t authorFirstNameSize = std::strlen(" Имя "),
+		authorLastNameSize = std::strlen(" Фамилия "),
+		bookTitleSize = std::strlen(" Заголовок "),
+		bookYearSize = std::strlen(" Год "),
+		bookPriceSize = std::strlen(" Цена "),
+		bookCategorySize = std::strlen(" Категория ");
+	for (BOOK book : this->bookVector) {
+		if (authorFirstNameSize < book.authorFirstName.size()) {
+			authorFirstNameSize = book.authorFirstName.size();
+		}
+		if (authorLastNameSize < book.authorLastName.size()) {
+			authorLastNameSize = book.authorLastName.size();
+		}
+		if (bookTitleSize < book.bookTitle.size()) {
+			bookTitleSize = book.bookTitle.size();
+		}
+		if (bookYearSize < std::to_string(bookYearSize).size()) {
+			bookYearSize = std::to_string(bookYearSize).size();
+		}
+		if (bookPriceSize < std::to_string(book.bookPrice).size()) {
+			bookPriceSize = std::to_string(book.bookPrice).size();
+		}
+		if (bookCategorySize < book.getBookCategory().size()) {
+			bookCategorySize = book.getBookCategory().size();
+		}
+	}
+	std::cout << alignCenter("Имя", authorFirstNameSize) << " | "
+		<< alignCenter("Фамилия", authorLastNameSize) << " | "
+		<< alignCenter("Заголовок", bookTitleSize) << " | "
+		<< alignCenter("Год", bookYearSize) << " | "
+		<< alignCenter("Цена", bookPriceSize) << " | "
+		<< alignCenter("Категория", bookCategorySize) << " |\n";
+	size_t totalSize = authorFirstNameSize + authorLastNameSize + bookTitleSize + bookYearSize + bookPriceSize + bookCategorySize;
+	totalSize += totalSize * 0.21;
+	for (size_t i=0; i <= totalSize; i++)
+		std::cout << "—";
+	std::cout << std::endl;
+	for (BOOK book : this->bookVector) {
+		std::cout << alignCenter(book.authorFirstName, authorFirstNameSize) << " | ";
+		std::cout << alignCenter(book.authorLastName, authorLastNameSize) << " | "
+			<< alignCenter(book.bookTitle, bookTitleSize) << " | "
+			<< alignCenter(std::to_string(book.bookYear), bookYearSize) << " | "
+			<< alignCenter(std::to_string(book.bookPrice), bookPriceSize) << " | "
+			<< alignCenter(book.getBookCategory().c_str(), bookCategorySize) << " |\n";
+	}
+}
+
+std::string alignCenter(const std::string s, const int w) {
+	std::stringstream ss, spaces;
+	int padding = w - s.size();
+	for (int i = 0; i < padding / 2; ++i)
+		spaces << " ";
+	ss << spaces.str() << s << spaces.str();
+	if (padding > 0 && padding % 2 != 0)
+		ss << " ";
+	return ss.str();
+}
+
+std::string alignRight(const std::string s, const int w) {
+	std::stringstream ss, spaces;
+	int padding = w - s.size();
+	for (int i = 0; i < padding; ++i)
+		spaces << " ";
+	ss << spaces.str() << s;
+	return ss.str();
+}
+
+std::string alignLeft(const std::string s, const int w) {
+	std::stringstream ss, spaces;
+	int padding = w - s.size();
+	for (int i = 0; i < padding; ++i)
+		spaces << " ";
+	ss << s << spaces.str();
+	return ss.str();
 }
--- other.h
+++ other.h
@@ -1,4 +1,10 @@
 
+#include <cstdio>
+#include <string>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+
 const char
 COMMAND_PRINT[] = "print",
 COMMAND_SET[] = "set",
@@ -9,4 +15,37 @@ COMMAND_HELP[] = "help",
 COMMAND_EXIT[] = "exit",
 COMMAND_ENDL[] = "\n";
 
+// run запускает выбор команд
 void run();
+
+// Card структура книжной карточки
+struct Card {
+	std::vector<BOOK> bookVector;
+
+	// Card конструктор, срабатывающий при инициализации экземпл€ра карты
+	Card() {
+
+		BOOK newBook;
+		newBook.authorFirstName.append("—ергей");
+		newBook.authorLastName.append("Ќиколаевич");
+		newBook.bookTitle.append(" ак научитс€ кодить на с++ за 14 дней?");
+		newBook.bookYear = 1990;
+		newBook.bookPrice = 28;
+		newBook.bookCategory = BOOK_CATEGORY_TRAVEL;
+
+		this->bookVector.push_back(newBook);
+	}
+
+	// set добавл€ет новую книгу в карточку
+	void set();
+
+	// print печатает содержимое карточки на экран
+	void print();
+};
+
+// alignCenter печатает содержимое €чейки табицы по центру
+std::string alignCenter(const std::string s, const int w);
+// alignCenter печатает содержимое €чейки табицы по правому краю
+std::string alignRight(const std::string s, const int w);
+// alignCenter печатает содержимое €чейки табицы с левого кра€
+std::string alignLeft(const std::string s, const int w);
